6496360d637fb225bb70fd5eff29e8c143545331
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-30 10:30:04 -0400

    Add verify passport strategy

--- authentication/passport.js
+++ authentication/passport.js
@@ -1,2 +1,29 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const getUserFromUsername = require('../db/queries');
+
+const customFields = {
+    usernameField: 'uname',
+    passwordField: 'pw'
+};
+
+async function verifyCallback(username, password, done) {
+    try {
+      const rows = getUserFromUsername(username);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+}
+
+const strategy = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
